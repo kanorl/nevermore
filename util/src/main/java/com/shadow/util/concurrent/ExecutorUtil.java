00475f4cbff52e7a92503b90366a476ad532bc81
kanorl <lzl4961@163.com>
2015-01-04 11:05:49 +0800

    hehe

--- util/src/main/java/com/shadow/util/concurrent/ExecutorUtil.java
+++ util/src/main/java/com/shadow/util/concurrent/ExecutorUtil.java
@@ -1,9 +1,10 @@
 package com.shadow.util.concurrent;
 
-import com.google.common.util.concurrent.MoreExecutors;
+import com.shadow.util.codec.JsonUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.TimeUnit;
 
@@ -13,11 +14,20 @@ import java.util.concurrent.TimeUnit;
 public class ExecutorUtil {
     private static final Logger LOGGER = LoggerFactory.getLogger(ExecutorUtil.class);
 
-    public static void shutdownAndAwaitTermination(ExecutorService service, String serviceName, long timeout, TimeUnit unit) {
+    public static boolean shutdownAndAwaitTermination(ExecutorService service, String serviceName, long timeout, TimeUnit unit) {
         LOGGER.info("开始[关闭{}线程池]", serviceName);
-        if (!MoreExecutors.shutdownAndAwaitTermination(service, timeout, unit)) {
-            LOGGER.error("[{}]线程池无法在规定时间内[{} {}]关闭", serviceName, timeout, unit);
+        service.shutdown();
+        try {
+            service.awaitTermination(timeout, unit);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
         }
-        LOGGER.info("完成[关闭{}线程池]", serviceName);
+        if (service.isTerminated()) {
+            LOGGER.info("完成[关闭{}线程池]", serviceName);
+        } else {
+            List<Runnable> tasks = service.shutdownNow();
+            LOGGER.error("[{}]线程池无法在规定时间内[{} {}]关闭，未执行的任务：", serviceName, timeout, unit, JsonUtil.toJson(tasks));
+        }
+        return service.isTerminated();
     }
 }
