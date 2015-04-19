e79ed11b74d87907c7aea0721d812f407951da9a
kanorl <lzl4916@163.com>
2015-04-19 13:35:12 +0800

    hehe

--- event/src/main/java/com/shadow/event/EventBus.java
+++ event/src/main/java/com/shadow/event/EventBus.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 import javax.annotation.Nonnull;
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
-import java.util.Set;
+import java.util.Collection;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -22,6 +22,7 @@ import static java.util.Objects.requireNonNull;
  *
  * @author nevermore on 2014/12/28.
  */
+@SuppressWarnings({"unchecked", "rawtypes"})
 @Component
 public class EventBus {
     private static final Logger LOGGER = LoggerFactory.getLogger(EventBus.class);
@@ -50,7 +51,7 @@ public class EventBus {
 
     public void post(@Nonnull Event event) {
         requireNonNull(event, "事件不能为null");
-        Set<EventListener<Event>> listeners = eventListenerManager.getListeners(event.getClass());
+        Collection<EventListener> listeners = eventListenerManager.getListeners(event.getClass());
         if (listeners.isEmpty()) {
             LOGGER.info("未被监听的事件类型[{}]", event.getClass().getName());
             return;
@@ -60,7 +61,7 @@ public class EventBus {
 
     public void syncPost(@Nonnull Event event) {
         requireNonNull(event, "事件不能为null");
-        Set<EventListener<Event>> listeners = eventListenerManager.getListeners(event.getClass());
+        Collection<EventListener> listeners = eventListenerManager.getListeners(event.getClass());
         if (listeners.isEmpty()) {
             LOGGER.info("未被监听的事件类型[{}]", event.getClass().getName());
             return;
--- event/src/main/java/com/shadow/event/EventListenerManager.java
+++ event/src/main/java/com/shadow/event/EventListenerManager.java
@@ -1,33 +1,61 @@
 package com.shadow.event;
 
+import com.google.common.collect.*;
 import com.shadow.common.util.lang.ReflectUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.config.BeanPostProcessor;
-import org.springframework.context.ApplicationListener;
-import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Nonnull;
+import javax.annotation.PostConstruct;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 import java.util.*;
 import java.util.stream.Collectors;
 
-import static java.util.Collections.unmodifiableSet;
-
 /**
- * 事件分发器
+ * 事件监听器管理
  *
  * @author nevermore on 2014/12/28.
  */
+@SuppressWarnings({"unchecked", "rawtypes"})
 @Component
-public class EventListenerManager implements BeanPostProcessor, ApplicationListener<ContextRefreshedEvent> {
+public class EventListenerManager {
     private static final Logger LOGGER = LoggerFactory.getLogger(EventListenerManager.class);
 
-    private Map<EventListener<Event>, Set<Class<? extends Event>>> listenerScope = new HashMap<>();
-    private Map<Class<? extends Event>, Set<EventListener<Event>>> eventScope;
+    @Autowired
+    private ApplicationContext ctx;
+
+    private Multimap<Class<? extends Event>, EventListener> listeners = ImmutableListMultimap.of();
+
+    @PostConstruct
+    private void init() {
+        Set<EventListener> allListeners = Sets.newHashSet(ctx.getBeansOfType(EventListener.class).values());
+        Map<EventListener, Set<Class<? extends Event>>> listenedEvents = Maps.asMap(allListeners, listener -> Arrays.stream(listener.getClass().getGenericInterfaces())
+                        .filter(type -> type instanceof ParameterizedType && EventListener.class.isAssignableFrom((Class<?>) ((ParameterizedType) type).getRawType()))
+                        .flatMap(type -> {
+                            Type[] pTypes = ((ParameterizedType) type).getActualTypeArguments();
+                            Class<Event> clazz = (Class<Event>) pTypes[0];
+                            Set<Class<? extends Event>> classes = ReflectUtil.getAllSubTypesOf(clazz);
+                            classes.add(clazz);
+                            return classes.stream();
+                        })
+                        .collect(Collectors.toSet())
+        );
+
+        Multimap<Class<? extends Event>, EventListener> eventListeners = ArrayListMultimap.create();
+        ReflectUtil.getAllSubTypesOf(Event.class).forEach(eventType -> {
+            List<EventListener> listeners = listenedEvents.keySet().stream().filter(key -> listenedEvents.get(key).contains(eventType)).collect(Collectors.toList());
+            eventListeners.putAll(eventType, listeners);
+
+            if (LOGGER.isDebugEnabled()) {
+                LOGGER.debug("事件[{}]的监听器: [{}]", eventType.getName(), listeners.stream().map(listener -> listener.getClass().getSimpleName()));
+            }
+        });
+        this.listeners = ImmutableListMultimap.copyOf(eventListeners);
+    }
 
     /**
      * 获取该事件的所有监听器
@@ -36,46 +64,7 @@ public class EventListenerManager implements BeanPostProcessor, ApplicationListe
      * @return a unmodifiable set
      */
     @Nonnull
-    Set<EventListener<Event>> getListeners(Class<? extends Event> eventType) {
-        if (eventScope == null) {
-            throw new IllegalStateException("事件模块未初始化完成");
-        }
-        return eventScope.getOrDefault(eventType, Collections.emptySet());
-    }
-
-    @SuppressWarnings("unchecked")
-    @Override
-    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
-        if (!(bean instanceof EventListener)) {
-            return bean;
-        }
-        Arrays.stream(bean.getClass().getGenericInterfaces())
-                .filter(type -> type instanceof ParameterizedType && EventListener.class.isAssignableFrom((Class<?>) ((ParameterizedType) type).getRawType()))
-                .forEach(type -> {
-                    Type[] pTypes = ((ParameterizedType) type).getActualTypeArguments();
-                    Class<Event> clazz = (Class<Event>) pTypes[0];
-                    Set<Class<? extends Event>> scope = ReflectUtil.getAllSubTypesOf(clazz);
-                    scope.add(clazz);
-                    listenerScope.put((EventListener<Event>) bean, scope);
-                });
-        return bean;
-    }
-
-    @Override
-    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
-        return bean;
-    }
-
-    @Override
-    public void onApplicationEvent(ContextRefreshedEvent event) {
-        Map<Class<? extends Event>, Set<EventListener<Event>>> eventScope = new HashMap<>();
-        ReflectUtil.getAllSubTypesOf(Event.class).forEach(eventType -> {
-            Set<EventListener<Event>> listeners = listenerScope.keySet().stream().filter(listener -> listenerScope.get(listener).contains(eventType)).collect(Collectors.toSet());
-            eventScope.put(eventType, unmodifiableSet(listeners));
-
-            LOGGER.info("事件[{}]的监听器: {}", eventType.getName(), listeners);
-        });
-        this.eventScope = Collections.unmodifiableMap(eventScope);
-        listenerScope = null;// release as it is useless
+    Collection<EventListener> getListeners(Class<? extends Event> eventType) {
+        return listeners.get(eventType);
     }
 }
